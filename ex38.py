09cf2cd2867bba290d45d372522b086a49f2378e
oktaran <oksana.taran7@gmail.com>
2018-05-03 12:34:57 +0300

    exercise 38

--- ex38.py
+++ ex38.py
@@ -6,11 +6,21 @@ stuff = ten_things.split(' ')
 more_stuff = ["Day", "Night", "Song", "Frisbee",
              "Corn", "Banana", "Girl", "Boy"]
 
-while len(stuff) != 10:
-    next_one = more_stuff.pop()
-    print("Adding: ", next_one)
-    stuff.append(next_one)
-    print(f"There are {len(stuff)} items now.")
+# while len(stuff) != 10:
+#     next_one = more_stuff.pop()
+#     print("Adding: ", next_one)
+#     stuff.append(next_one)
+#     print(f"There are {len(stuff)} items now.")
+
+
+for l in range (0, 11):
+    l = len(stuff)
+    if l < 10:
+        next_one = more_stuff.pop()
+        print("Adding: ", next_one)
+        stuff.append(next_one)
+        print(f"There are {len(stuff)} items now.")
+
 
 print(f"There we go: ", stuff)
 
